8f5e0a715c1a974aa1a6e7532f14cd77bc80fdf8
amira <amirazenhom88@gmail.com>
2023-12-26 21:33:29 +0200

    header

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -3,7 +3,10 @@ export default function Home() {
     
     return (
         <>
-            <h1 className="text-center">Homeeeeeeeee</h1>
+             <div className="bgHome text-white py-5 px-4 mx-3">
+                    <h1 className="my-3">Welcome <span className="text1">Upskilling</span></h1>
+                    <p className="my-3 fs-2">You can add project and assign tasks to your team</p>
+                   </div>
         </>
     )
 }
